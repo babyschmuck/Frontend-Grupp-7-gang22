561dc85b87d685d472963edc69a86b7da7717639
mattekiwi <kivimatte@gmail.com>
2023-04-27 15:32:52 +0200

    small fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import LogIn from './components/views/LogIn';
 import './App.css';
+import './components/partials/LogInPartial';
 
 function App() {
   return (
   <>
- <LoginPartial/>
+  <LogIn />
   </>
   );
 }
