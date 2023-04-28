b69f0b5d549ca98ad7eaac704c39a1cd340e5b47
Your Name <you@example.com>
2023-04-28 12:14:27 +0200

    loginview

--- src/components/partials/RegisterPartial.js
+++ src/components/partials/RegisterPartial.js
@@ -1,14 +1,68 @@
+import React, { useState } from "react";
 
 const RegisterPartial = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-return(<div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle registration logic
+  };
 
+  return (
+    <form onSubmit={handleSubmit} className="register-form">
+      <h2>Create Account</h2>
+      <p>Sign up to get started!</p>
 
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
 
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
 
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
 
+      <button type="submit" className="btn btn-theme">
+        SIGN UP
+      </button>
 
-</div>)
+      <div className="signup-link">
+        Already have an account? <a href="#">Sign in</a>
+      </div>
+    </form>
+  );
+};
 
-}
-export default RegisterPartial
+export default RegisterPartial;
