1f961f250fd06e9a108702ceea24106dafa0a551
mattekiwi <kivimatte@gmail.com>
2023-05-03 11:29:30 +0200

    headerFix

--- src/components/partials/HeaderPartial.js
+++ src/components/partials/HeaderPartial.js
@@ -1,30 +1,31 @@
-import React, { useState } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const HeaderPartial = () => {
-    const [hideMenu, setHideMenu] = useState(true)
+  const [hideMenu, setHideMenu] = useState(true);
 
   return (
     <header>
-        <div className="container">
-            <Link id="logotype" to="/">Manero.</Link> 
-            <div id="menu" className={hideMenu ? 'hide' : false}>
-                <nav className="menu-links links">
-                    <NavLink className="link" to="/">Home</NavLink>
-                </nav>
-                <nav className="menu-icons icons">
-                    <NavLink className="link" to="/search"><i className="fa-regular fa-search"></i></NavLink>
-                    <NavLink className="link" to="/favorites"><i className="fa-regular fa-heart"></i></NavLink>
-                    <NavLink className="link" to="/cart"><i className="fa-regular fa-bag-shopping"></i></NavLink>
-                    <NavLink className="link ms-5" to="/account"><i className="fa-regular fa-user"></i></NavLink>
-                </nav>
-            </div>
-            <div id="toggle-icon" className="icons">
-                <button onClick={() => setHideMenu(!hideMenu)} className="link"><i className="fa-regular fa-bars"></i></button>
-            </div>
+      <div className="container">
+        <Link id="logotype" to="/">
+          Manero.
+        </Link>
+        <div id="menu" className={`menu ${hideMenu ? 'hide' : ''}`}>
+          <nav className="menu-icons icons">
+            <NavLink className="link" to="/search"><i className="fa-regular fa-search"></i></NavLink>
+            <NavLink className="link" to="/favorites"><i className="fa-regular fa-heart"></i></NavLink>
+            <NavLink className="link" to="/cart"><i className="fa-regular fa-bag-shopping"></i></NavLink>
+            <NavLink className="link ms-5" to="/account"><i className="fa-regular fa-user"></i></NavLink>
+          </nav>
         </div>
+        <div id="toggle-icon" className="icons">
+          <button onClick={() => setHideMenu(!hideMenu)} className="link">
+            <i className="fa-regular fa-bars"></i>
+          </button>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default HeaderPartial
+export default HeaderPartial;
