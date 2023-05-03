12f17bbca5514f22a05945c2a82bdf09082de978
mattekiwi <kivimatte@gmail.com>
2023-05-03 10:08:14 +0200

    ye

---
+++ src/components/partials/FooterPartial.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function FooterPartial() {
+  return (
+    <>
+    <footer>
+        <div className="container"> 
+        
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default FooterPartial
