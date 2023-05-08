b59df8550164586e836a126a67ba9fdd55403585
Your Name <you@example.com>
2023-05-08 18:10:07 +0200

    welcome(onboarding1)

---
+++ src/components/partials/OnBoardingPartial1.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+function OnBoardingPartial1() {
+    return (
+        <div>
+            <div className='upper-half'>
+
+            </div>
+            <div className='lower-half'>
+                <div className='container'>
+                <div className='circle'>
+                    <div className='title'>Welcome to Manero!</div>
+                    <div className='description'>Let us get you started with Manero!</div>
+                   
+                    <button className='startbutton'>
+                        <div className='buttontext'>GET STARTED</div>
+                    </button>
+                </div>
+                
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+
+
+
+
+      );
+    }
+
+
+export default OnBoardingPartial1;
---
+++ src/components/views/Welcome.js
@@ -0,0 +1,18 @@
+import OnBoardingPartial1 from "../partials/OnBoardingPartial1";
+
+
+const Welcome = () => {
+
+return(<>
+
+
+<OnBoardingPartial1/>
+
+</>)
+
+
+
+
+
+}
+export default Welcome;
